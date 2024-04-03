7f83300d1cd54c5fae0f455b5fab1b3a9b023bcc
Aniketh <aaatipamula@gmail.com>
2024-04-03 17:31:01 -0500

    update for public html

--- table.php
+++ table.php
@@ -5,8 +5,16 @@
   <title>Practice 4</title>
   <meta name="viewport" content="width=device-width,initial-scale=1" />
   <style type="text/css">
+    table {
+      border-collapse: collapse;
+    }
+    table, tr, td {
+      border: 2px solid;
+    }
     td {
-      padding: .5rem;
+      width: 2rem;
+      height: 2rem;
+      text-align: center;
     } 
   </style>
 </head>
@@ -21,11 +29,11 @@
 
   for ($row = 0; $row <= $size; $row++) {
     echo "<tr>";
-      for ($col= 1; $col <= $size; $col++) {
+      for ($col= 0; $col <= $size; $col++) {
         if ($row == 0) {
-          print "<td>" . $row . "</td>";
-        } else if ($col == 0) {
           print "<td>" . $col . "</td>";
+        } else if ($col == 0) {
+          print "<td>" . $row . "</td>";
         } else {
           print "<td>" . ($row * $col) . "</td>";
         }
